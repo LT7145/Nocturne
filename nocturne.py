2c342ded3533e1819171f1a10e4bc6636420bfb3
LayTun <ltun01@syr.edu>
2026-08-23 19:56:57 -0400

    pngs

---
+++ nocturne.py
@@ -0,0 +1,9 @@
+import argparse
+import sys
+
+def main():
+    parse = argparse.ArgumentParser(
+            prog="nocturne"
+            description="Nocturne - Automated Recon Pipeline"
+            )
+    parser.add_argument
